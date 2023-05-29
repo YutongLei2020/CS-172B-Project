21d506ee83bed14d68d6c69bf1d0860f9017b82f
Dylan Chen <93622599+MeB4You@users.noreply.github.com>
2023-05-28 18:27:21 -0700

    Update ResNet.py

--- ResNet.py
+++ ResNet.py
@@ -55,12 +55,11 @@ def ResidualBlock(x, filters, kernel_size, weight_decay, downsample=True):
 
 
 def ResNet18(classes, input_shape, weight_decay=1e-4):
+    """For fer2013 """
     input = Input(shape=input_shape)
     x = input
-#     x = conv2d_bn_relu(x, filters=64, kernel_size=(7, 7), weight_decay=weight_decay, strides=(2, 2)) # this is for real resnet18
     x = conv2d_bn_relu(x, filters=64, kernel_size=(3, 3), weight_decay=weight_decay, strides=(2, 2))
     x = MaxPool2D(pool_size=(3, 3), strides=(2, 2),  padding='same')(x)
-#     x = conv2d_bn_relu(x, filters=64, kernel_size=(3, 3), weight_decay=weight_decay, strides=(1, 1)) # this is random stuff
 
     # # conv 2
     x = ResidualBlock(x, filters=64, kernel_size=(3, 3), weight_decay=weight_decay, downsample=False)
@@ -74,44 +73,9 @@ def ResNet18(classes, input_shape, weight_decay=1e-4):
     # # conv 5
     x = ResidualBlock(x, filters=512, kernel_size=(3, 3), weight_decay=weight_decay, downsample=True)
     x = ResidualBlock(x, filters=512, kernel_size=(3, 3), weight_decay=weight_decay, downsample=False)
-#     x = AveragePooling2D(pool_size=(4, 4), padding='valid')(x) # this is for real resnet18 with inputsize 224x224
     x = AveragePooling2D(pool_size=(2, 2), padding='valid')(x)
     x = Flatten()(x)
     x = Dense(classes, activation='softmax')(x)
     model = Model(input, x, name='ResNet18')
     return model
 
-
-def ResNetForCIFAR10(classes, name, input_shape, block_layers_num, weight_decay):
-    input = Input(shape=input_shape)
-    x = input
-    x = conv2d_bn_relu(x, filters=16, kernel_size=(3, 3), weight_decay=weight_decay, strides=(1, 1))
-
-    # # conv 2
-    for i in range(block_layers_num):
-        x = ResidualBlock(x, filters=16, kernel_size=(3, 3), weight_decay=weight_decay, downsample=False)
-    # # conv 3
-    x = ResidualBlock(x, filters=32, kernel_size=(3, 3), weight_decay=weight_decay, downsample=True)
-    for i in range(block_layers_num - 1):
-        x = ResidualBlock(x, filters=32, kernel_size=(3, 3), weight_decay=weight_decay, downsample=False)
-    # # conv 4
-    x = ResidualBlock(x, filters=64, kernel_size=(3, 3), weight_decay=weight_decay, downsample=True)
-    for i in range(block_layers_num - 1):
-        x = ResidualBlock(x, filters=64, kernel_size=(3, 3), weight_decay=weight_decay, downsample=False)
-    x = AveragePooling2D(pool_size=(8, 8), padding='valid')(x)
-    x = Flatten()(x)
-    x = Dense(classes, activation='softmax')(x)
-    model = Model(input, x, name=name)
-    return model
-
-
-def ResNet20ForCIFAR10(classes, input_shape, weight_decay):
-    return ResNetForCIFAR10(classes, 'resnet20', input_shape, 3, weight_decay)
-
-
-def ResNet32ForCIFAR10(classes, input_shape, weight_decay):
-    return ResNetForCIFAR10(classes, 'resnet32', input_shape, 5, weight_decay)
-
-
-def ResNet56ForCIFAR10(classes, input_shape, weight_decay):
-    return ResNetForCIFAR10(classes, 'resnet56', input_shape, 9, weight_decay)
